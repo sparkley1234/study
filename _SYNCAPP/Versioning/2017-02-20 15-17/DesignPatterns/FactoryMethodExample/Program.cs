16ba911efcceb45598e7e01a4e4457b13578ec3c
sparkley1234 <sparkley1234@gmail.com>
2017-02-20 15:18:01 +0530

    / ‘DesignPatterns/FactoryMethodExample/Program.cs’ + ‘_SYNCAPP/Versioning/2017-02-20 15-17/DesignPatterns/FactoryMethodExample/Program.cs’ / ‘_SYNCAPP/metadata.xml’

--- _SYNCAPP/Versioning/2017-02-20 15-17/DesignPatterns/FactoryMethodExample/Program.cs
+++ _SYNCAPP/Versioning/2017-02-20 15-17/DesignPatterns/FactoryMethodExample/Program.cs
@@ -8,22 +8,22 @@ using System.Data;
 namespace FactoryMethodExample
     {
         // 'IProduct' Interface
-        interface IEmployee
-    {
+        interface IFactory
+        {
             void details();
         }
 
         // 'ConcreteProduct' class
-        class PermanentEmployee : IEmployee
-    {
+        class PermanentEmployee : IFactory
+        {
             public void details()
             {
                 Console.WriteLine("This is permanent employee type object");
             }
         }
         // 'ConcreteProduct' class
-        class TemporaryEmployee : IEmployee
-    {
+        class TemporaryEmployee : IFactory
+        {
             public void details()
             {
                 Console.WriteLine("This is Temporary employee type object");
@@ -32,7 +32,7 @@ namespace FactoryMethodExample
 
         class EmployeeFactory
         {
-            public IEmployee Factory(Employee type)
+            public IFactory Factory(Employee type)
             {
                 switch (type)
                 {
@@ -47,19 +47,19 @@ namespace FactoryMethodExample
         }
     class EmployeeFactoryByReflection
     {
-        public IEmployee Factory(Employee stype)
+        public IFactory Factory(Employee stype)
         {
-            IEmployee IF = null;
+            IFactory IF = null;
             try
             {
                 Type type = Type.GetType(stype.ToString());
                 if (type != null)
-                    return (IEmployee)Activator.CreateInstance(type);
+                    return (IFactory)Activator.CreateInstance(type);
                 foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
                 {
                     type = asm.GetType("FactoryMethodExample.PermanentEmployee");
                     if (type != null)
-                        return (IEmployee)Activator.CreateInstance(type);
+                        return (IFactory)Activator.CreateInstance(type);
                 }
                 return IF;
             }
@@ -82,19 +82,19 @@ namespace FactoryMethodExample
     // calling class/ client
     class Program
         {
-        //static void Main(string[] args)
-        //{
-        //    EmployeeFactory EF = new EmployeeFactory();
-        //    IEmployee permanentEmployee = EF.Factory(Employee.PermanentEmployee);
-        //    permanentEmployee.details();
+            //static void Main(string[] args)
+            //{
+            //    EmployeeFactory EF = new EmployeeFactory();
+            //    IFactory permanentEmployee = EF.Factory(Employee.PermanentEmployee);
+            //    permanentEmployee.details();
 
-        //    IEmployee temporaryEmployee = EF.Factory(Employee.TemporaryEmployee);
-        //    temporaryEmployee.details();
-        //    EmployeeFactoryByReflection EFR = new EmployeeFactoryByReflection();
-        //    IEmployee permanentEmployeeByReflection = EFR.Factory(Employee.PermanentEmployee);
+            //    IFactory temporaryEmployee = EF.Factory(Employee.TemporaryEmployee);
+            //    temporaryEmployee.details();
+            //    EmployeeFactoryByReflection EFR = new EmployeeFactoryByReflection();
+            //    IFactory permanentEmployeeByReflection = EFR.Factory(Employee.PermanentEmployee);
 
-        //    Console.ReadLine();
-        //}
+            //    Console.ReadLine();
+            //}
+        }
     }
-}
 
